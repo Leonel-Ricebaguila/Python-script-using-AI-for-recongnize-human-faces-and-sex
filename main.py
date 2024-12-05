35a0d98c1ec1792ecc4cdda61cc37b6b6f91ce71
KameosOrg <reismansebastian@gmail.com>
2024-12-05 13:29:26 -0600

    I added leo Sexuality Identifier

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 #Import libraries
-import opencv as cv
+
+import time
+import cv2 as cv
 import numpy as numpy
+import leoSexualityIdentifier
+print("1")
+print(leoSexualityIdentifier.LOL)
+print("2")
