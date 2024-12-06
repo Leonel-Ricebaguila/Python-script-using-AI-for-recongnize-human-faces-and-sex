d9fbd3203a92feb1c3fc5728216441d6d5e6a88d
KameosOrg <reismansebastian@gmail.com>
2024-12-06 00:21:52 -0600

    modelos

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ print("1")
 fps = 5
 defaultWebCam = 0 # 0 communmente es la default
 pressedKey = 255
+genders = ["    Male", "Female"]
 videoCapturado = computerVision.VideoCapture(defaultWebCam) # captura objeto en videoEnVivo
 face_cascade = computerVision.CascadeClassifier(computerVision.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# modelos de detexion de cara
+gender_model = "gender_net.caffemodel"
+gender_proto = "gender_deploy.prototxt"
+
+# creacion de estos
+
+gender_net = computerVision.dnn.readNetFromCaffe(gender_proto, gender_model)
 
 while True:
 
@@ -23,9 +32,23 @@ while True:
     for (x, y, w, h) in faces:
         computerVision.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
 
+    face = frame[y : y + h, x : x + w]
+    blob_face = computerVision.dnn.blobFromImage(face, 1.0, (227, 227),(78.4263377603, 87.7689143744, 114.895847746))
+    gender_net.setInput(blob_face)
+    
+    gender_preds = gender_net.forward()
+    gender = genders[gender_preds[0].argmax()]
+    
+    if gender.strip() == "Male":
+        color = (0, 255, 0)
+    else:
+        color = (255, 0, 255)
+
+    computerVision.putText(frame, gender, (x, y - 10), computerVision.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+
     computerVision.imshow("LeoGayCam", frame) # actualiza el window
     pressedKey = computerVision.waitKey(1000//fps) & 0xFF
-      
+
     # permite controlar el programa con teclas
     if (funciones.stopCapturing(pressedKey)): # sale del programa si se presiona q
         break
