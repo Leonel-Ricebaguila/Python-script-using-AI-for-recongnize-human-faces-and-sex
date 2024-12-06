db1ca8ef421e7d47771931e9035ad4c63cdc3dcf
KameosOrg <reismansebastian@gmail.com>
2024-12-05 23:23:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 #Import libraries
 
-import time
 import cv2 as computerVision
 import numpy as numpy
-import keyboard
 import funciones
 
 print("1")
@@ -13,10 +11,18 @@ fps = 5
 defaultWebCam = 0 # 0 communmente es la default
 pressedKey = 255
 videoCapturado = computerVision.VideoCapture(defaultWebCam) # captura objeto en videoEnVivo
+face_cascade = computerVision.CascadeClassifier(computerVision.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 while True:
 
     capturaExitosa, frame =  videoCapturado.read() # agarra un boolean (el exito) y un array (el frame)
+
+    gray = computerVision.cvtColor(frame, computerVision.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        computerVision.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
+
     computerVision.imshow("LeoGayCam", frame) # actualiza el window
     pressedKey = computerVision.waitKey(1000//fps) & 0xFF
       
